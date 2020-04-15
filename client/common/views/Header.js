0017543154514c54c8f3dd6dedd80c31a4ab25b6
Lukas Karcher <32646371+kr4cher@users.noreply.github.com>
2020-04-15 19:09:45 +0200

    styling

--- client/common/views/Header.js
+++ client/common/views/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import Logo from '../styles/libp2p-logo.svg'
+import Logo from '../styles/ipfs-logo.png'
 
 export default function Header ({
   started
 }) {
   return (
     <header className='flex items-center pa2 bg-dark-blue'>
-      <a href='https://libp2p.io' title='home' className='w-50'>
+      <a href='https://ipfs.io/' title='home' className='w-50' target='_blank'>
         <img className={started ? 'libp2p-on' : 'libp2p-off' } src={Logo} style={{ height: 50 }} />
       </a>
-      <h1 className='w-50 ma0 tr f3 fw2 nexa white'><span className='fw7'>libp</span>p2p chat</h1>
+      <h1 className='w-50 ma0 tr f3 fw2 nexa white'><span className='fw7'>p2p</span> chat</h1>
     </header>
   )
 }
